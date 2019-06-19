561724a3e7148f61db621832b9e671f961406ef6
Woeler <woeler@woeler.eu>
2019-06-19 17:26:33 +0200

    [TASK] Persist and retain folder path parameter

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 const electron = require('electron');
 const path = require('path');
+const storage = require('electron-json-storage');
 const BrowserWindow = electron.remote.BrowserWindow;
 const remote = electron.remote;
 
 const chooseBtn = document.getElementById('select-folder');
 const placeHolder = document.getElementById('wow-folder');
+
+placeHolder.value = storage.get('folder', null);
+storage.get('folderPath', function(error, data) {
+    if (error) throw error;
+
+    if (undefined !== data['folder'] && null !== data['folder']) {
+        placeHolder.value = data['folder'];
+    }
+});
 
 chooseBtn.addEventListener('click', function(event) {
     remote.dialog.showOpenDialog(
@@ -13,6 +23,11 @@ chooseBtn.addEventListener('click', function(event) {
             properties: ["openDirectory"]
         }, (folderPath) => {
             placeHolder.setAttribute('placeholder', folderPath);
+            console.log(folderPath);
+            storage.set('folderPath', { folder: folderPath }, function(error) {
+                if (error) throw error;
+                console.log("stored");
+            });
         }
     );
 });
