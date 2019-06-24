58206a69bd1ec4fdcfc95c00d82190231eb4ad3b
Woeler <woeler@woeler.eu>
2019-06-24 12:12:58 +0200

    [TASK] Add select boxes per account

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const fs = require('fs');
 
 const chooseBtn = document.getElementById('select-folder');
 const placeHolder = document.getElementById('wow-folder');
+const container = document.getElementById('settings');
 
 placeHolder.value = storage.get('folder', null);
 storage.get('folderPath', function (error, data) {
@@ -23,11 +24,41 @@ chooseBtn.addEventListener('click', function (event) {
             title: 'Select a folder',
             properties: ["openDirectory"]
         }, (folderPath) => {
+            if (undefined === folderPath) {
+                return;
+            }
             placeHolder.value = folderPath;
             storage.set('folderPath', {folder: folderPath}, function (error) {
                 if (error) throw error;
                 storage.set('accountFolders', {folders: getDirectories(folderPath)}, function (error) {
                     if (error) throw error;
+                    storage.get('accountFolders', function (error, storageFolders) {
+                        if (undefined === storageFolders['folders'] || null === storageFolders['folders']) {
+                            return;
+                        }
+
+                        for (let i = 0; i < storageFolders['folders'].length; i++) {
+                            var checkbox = document.createElement('input');
+                            checkbox.type = "checkbox";
+                            checkbox.name = storageFolders['folders'][i];
+                            checkbox.value = "value";
+                            checkbox.id = storageFolders['folders'][i];
+
+                            var label = document.createElement('label');
+                            label.htmlFor = storageFolders['folders'][i];
+                            label.appendChild(document.createTextNode(storageFolders['folders'][i]));
+
+                            container.appendChild(checkbox);
+                            container.appendChild(label);
+                            container.appendChild(document.createElement('br'));
+
+                            document.getElementById(storageFolders['folders'][i]).addEventListener('change', function () {
+                                if (document.getElementById(storageFolders['folders'][i]).checked) {
+                                    console.log(document.getElementById(storageFolders['folders'][i]).name);
+                                }
+                            });
+                        }
+                    });
                 });
             });
         }
